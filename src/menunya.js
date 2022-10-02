b2f0733332de81a64bee7676232e9ca22e73db52
Nell-XD <jhabwk@gmail.com>
2022-10-02 22:35:44 +0700

    master

---
+++ src/menunya.js
@@ -0,0 +1,31 @@
+const menunya = `
+╭━━•›ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙‹•━━╮
+┃╭┈─────────────⩵꙰ཱི࿐
+┃╰── ⏤͟͟͞MENU NYA ──➤ ↶↷
+╰━━•›ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙‹•━━͙✩̣̣̣̣
+ ▬▭▬▭▬ ✦✧✦ ▬▭▬▭▬
+╭━━•›〘 ALL MENU 〙
+𖠀⩵꙰ཱི࿐ BUGMENU 
+𖠀⩵꙰ཱི࿐ FUNMENU 
+𖠀⩵꙰ཱི࿐ MAINMENU 
+𖠀⩵꙰ཱི࿐ VOICEMENU 
+𖠀⩵꙰ཱི࿐ GROUPMENU 
+𖠀⩵꙰ཱི࿐ OWNERMENU 
+𖠀⩵꙰ཱི࿐ ISLAMICMENU 
+𖠀⩵꙰ཱི࿐ SEARCHMENU 
+𖠀⩵꙰ཱི࿐ EPHOTOMENU 
+𖠀⩵꙰ཱི࿐ RANDOMMENU 
+𖠀⩵꙰ཱི࿐ TEKSPROMENU 
+𖠀⩵꙰ཱི࿐ PRIMBONMENU 
+𖠀⩵꙰ཱི࿐ CONVERTMENU 
+𖠀⩵꙰ཱི࿐ WEBZONEMENU
+𖠀⩵꙰ཱི࿐ DATABASEMENU 
+𖠀⩵꙰ཱི࿐ PHOTOOXYMENU
+𖠀⩵꙰ཱི࿐ DOWNLOADMENU
+𖠀⩵꙰ཱི࿐ ANONYMOUSMENU
+╰━ ━ ━ ━ ━ ━ ━ ━ ━ ━•⩵꙰ཱི࿐
+ ▬▭▬▭▬ ✦✧✦ ▬▭▬▭▬
+
+© ⏤͟͟͞𝙏𝙃𝙀 𝘿𝘼𝙉𝙕 𝘽𝙊𝙏𝙕
+`
+exports.menunya = menunya
